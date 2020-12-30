e5a4bf858da1197ae42ac1f0c7312c0a7198e14a
Yun Ding <71709525+yun-ding@users.noreply.github.com>
2020-12-30 15:44:35 +0800

    Add files via upload

---
+++ app/src/main/java/com/laioffer/newsplus/ui/home/HomeViewModel.java
@@ -0,0 +1,34 @@
+package com.laioffer.newsplus.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+import androidx.lifecycle.ViewModel;
+
+import com.laioffer.newsplus.model.Article;
+import com.laioffer.newsplus.model.NewsResponse;
+import com.laioffer.newsplus.repository.NewsRepository;
+
+public class HomeViewModel extends ViewModel {
+    private final NewsRepository repository;
+    private final MutableLiveData<String> countryInput = new MutableLiveData<>();
+
+    public HomeViewModel(NewsRepository newsRepository) {
+        this.repository = newsRepository;
+    }
+
+    //view -> set ViewModel
+    public void setCountryInput(String country) {
+        countryInput.setValue(country);
+    }
+
+    public LiveData<NewsResponse> getTopHeadlines() {
+        return Transformations.switchMap(countryInput, repository::getTopHeadlines);
+    }
+
+    public void setFavoriteArticleInput(Article article) {
+        repository.favoriteArticle(article);
+    }
+
+
+}
