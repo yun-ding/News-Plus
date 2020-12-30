04e0d42645d04151dcb472ba2cb3fe8689f29a6f
Yun Ding <71709525+yun-ding@users.noreply.github.com>
2020-12-30 15:43:56 +0800

    Create DetailsFragment.java

---
+++ app/src/main/java/com/laioffer/newsplus/ui/details/DetailsFragment.java
@@ -0,0 +1,55 @@
+package com.laioffer.newsplus.ui.details;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laioffer.newsplus.R;
+import com.laioffer.newsplus.databinding.FragmentDetailsBinding;
+import com.laioffer.newsplus.model.Article;
+import com.squareup.picasso.Picasso;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DetailsFragment} factory method to
+ * create an instance of this fragment.
+ */
+public class DetailsFragment extends Fragment {
+    private FragmentDetailsBinding binding;
+
+    public DetailsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentDetailsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Article article = DetailsFragmentArgs.fromBundle(getArguments()).getArticle();
+        binding.detailsTitleTextView.setText(article.title);
+        binding.detailsAuthorTextView.setText(article.author);
+        binding.detailsDateTextView.setText(article.publishedAt);
+        binding.detailsDescriptionTextView.setText(article.description);
+        binding.detailsContentTextView.setText(article.content);
+        Picasso.get().load(article.urlToImage).into(binding.detailsImageView);
+    }
+}
