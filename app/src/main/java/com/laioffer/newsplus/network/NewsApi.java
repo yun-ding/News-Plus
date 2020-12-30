c32f3c0f6a182f77fbf2515bcecf95d6db5e4dfb
Yun Ding <71709525+yun-ding@users.noreply.github.com>
2020-12-30 15:42:27 +0800

    Create NewsApi.java

---
+++ app/src/main/java/com/laioffer/newsplus/network/NewsApi.java
@@ -0,0 +1,17 @@
+package com.laioffer.newsplus.network;
+
+import com.laioffer.newsplus.model.NewsResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface NewsApi {
+    @GET("top-headlines")
+    Call<NewsResponse> getTopHeadlines(@Query("country") String country);
+
+    @GET("everything")
+    Call<NewsResponse> getEverything(
+            @Query("q") String query, @Query("pageSize") int pageSize);
+
+}
