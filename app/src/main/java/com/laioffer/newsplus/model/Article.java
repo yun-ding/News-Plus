a906482337fc8ec45cee8b75d0b386bf49f04247
Yun Ding <71709525+yun-ding@users.noreply.github.com>
2020-12-30 15:41:40 +0800

    Create Article.java

---
+++ app/src/main/java/com/laioffer/newsplus/model/Article.java
@@ -0,0 +1,54 @@
+package com.laioffer.newsplus.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Article implements Serializable {
+    public String author;
+    public String title;
+    public String description;
+    public String publishedAt;
+    public String content;
+
+    @NonNull
+    @PrimaryKey
+    public String url;
+    public String urlToImage;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article)) return false;
+        Article article = (Article) o;
+        return Objects.equals(author, article.author) &&
+                Objects.equals(title, article.title) &&
+                Objects.equals(description, article.description) &&
+                Objects.equals(url, article.url) &&
+                Objects.equals(urlToImage, article.urlToImage) &&
+                Objects.equals(publishedAt, article.publishedAt) &&
+                Objects.equals(content, article.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, title, description, url, urlToImage, publishedAt, content);
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                ", author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", url='" + url + '\'' +
+                ", urlToImage='" + urlToImage + '\'' +
+                ", publishedAt='" + publishedAt + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
